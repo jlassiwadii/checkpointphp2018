14dcbb708a22ea5b28059dee97542dcdba8ddab0
wadii jlassi <39508603+jlassiwadii@users.noreply.github.com>
2018-11-16 14:33:09 +0100

    Add files via upload

--- php-session/page1.php
+++ php-session/page1.php
@@ -2,9 +2,10 @@
 
 session_start();
 
+
 echo"<p align=center'>compter le nombre des visites</p>"."<br>";
 
-if(empty($_SESSION['counter']))
+if(!isset($_SESSION['counter']))
 
 $_SESSION['counter']=1;
 else
@@ -12,8 +13,12 @@ else
 
 echo $_SESSION['counter']."<br>";
 echo '<br>';
+
+
+
 ?>
 
-<a href="page2.php"><input type="submit" name="exit" value="remise à 0"></a>
 
-
+<form method="post" action="page2.php">
+   <button type="submit" name="ref">remise à 0</button>
+</form>
--- php-session/page2.php
+++ php-session/page2.php
@@ -2,10 +2,6 @@
 session_start();
 
 
-echo"<p align=center'>Le dernier  nombre des visites est :</p>"."<br>";
-echo $_SESSION['counter']."<br>";
-
-
 echo"<p align=center'> nombre des visites remise à 0</p>"."<br>";
 if (!empty($_SESSION['counter']))
 $_SESSION['counter']=0;
